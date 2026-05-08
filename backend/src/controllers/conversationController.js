c45db4483cf9194ea581e273b9ce05d6195e6296
NhatQuang <nhatnguyen070706@gmail.com>
2026-05-08 22:44:34 +0700

    fix: update conversation deletion route parameter for clarity

--- backend/src/controllers/conversationController.js
+++ backend/src/controllers/conversationController.js
@@ -1,5 +1,6 @@
 const Conversation = require('../models/Conversation');
 const User = require('../models/User');
+const Message = require('../models/Message');
 const mongoose = require('mongoose');
 
 // Chuyển danh sách identifier (Username/Email/ID) thành danh sách ObjectIDs
